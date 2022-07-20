7e5c9e3a6dab3159ae5af64256739757a3a96e97
Alexander Meschaninov <64168998+ReachetatiVe@users.noreply.github.com>
2022-07-20 15:52:51 +0300

    overlay project list

--- src/components/overlayMenu/OverlayMenu.vue
+++ src/components/overlayMenu/OverlayMenu.vue
@@ -1,18 +1,24 @@
 <template>
-  <v-list>
-    <v-list-item-group v-model="model">
-      <v-list-item v-for="(item, i) in items" :key="i">
-        <v-list-item-icon>
-          <v-icon v-text="item.icon"></v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title v-text="item.text"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list-item-group>
-  </v-list>
+  <div>
+    <v-list>
+      <v-list-item-group v-model="model">
+        <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    <v-divider></v-divider>
+    <ProjectList/>
+  </div>
 </template>
 <script>
+import ProjectList from './OverlayProjectList.vue'
+
 export default {
   data: () => ({
     items: [
@@ -35,5 +41,8 @@ export default {
     ],
     model: 1,
   }),
+  components: {
+    ProjectList
+  }
 };
 </script>
---
+++ src/components/overlayMenu/OverlayProjectList.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-header>
+        <h3>Projects</h3>
+        <div>
+          <v-btn
+            elevation="2"
+            icon
+            x-small
+            class="mx-2"
+            fab
+            dark
+            color="indigo"
+            @click.stop="showOverlay = true"
+            ><v-icon hover>mdi-plus</v-icon>
+          </v-btn>
+        </div>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-list>
+          <v-list-item
+            v-for="project in getProjects"
+            :key="'_' + project.id"
+            link
+            ><Project
+              v-bind:info="project"
+              v-on:toggleProjectBrowser="toggleShowProjectBrowser"
+            />
+          </v-list-item>
+        </v-list>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
