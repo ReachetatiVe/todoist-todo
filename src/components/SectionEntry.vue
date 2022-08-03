4abf1a6429f197686513fdcc40652f15938aecc6
Alexander Meschaninov <64168998+ReachetatiVe@users.noreply.github.com>
2022-08-03 12:02:43 +0300

    subtask ui

--- src/components/SectionEntry.vue
+++ src/components/SectionEntry.vue
@@ -4,14 +4,17 @@
       <div class="text-h6">{{ section.name }}</div>
     </v-expansion-panel-header>
     <v-expansion-panel-content>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      <!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
+      commodo consequat. -->
+      <Task />
     </v-expansion-panel-content>
   </v-expansion-panel>
 </template>
 <script>
+import Task from "./TaskEntry.vue";
+
 export default {
   props: {
     section: {
@@ -21,5 +24,9 @@ export default {
   },
 
   data: () => ({}),
+
+  components: {
+    Task,
+  },
 };
 </script>
