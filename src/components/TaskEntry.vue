97f9d4a19694d6dc9d67c84ddd388a7b56c6f83b
Alexander Meschaninov <64168998+ReachetatiVe@users.noreply.github.com>
2022-08-03 12:15:06 +0300

    change subtask

--- src/components/TaskEntry.vue
+++ src/components/TaskEntry.vue
@@ -1,55 +1,41 @@
 <template>
   <v-list>
-    <v-list-group :value="true" prepend-icon="mdi-calendar-check">
+    <v-list-group :value="true" no-action sub-group>
       <template v-slot:activator>
-        <v-list-item-title>Task</v-list-item-title>
+        <v-list-item-content>
+          <v-list-item-title>Subtask1</v-list-item-title>
+        </v-list-item-content>
       </template>
 
-      <v-list-group :value="true" no-action sub-group>
-        <template v-slot:activator>
-          <v-list-item-content>
-            <v-list-item-title>Subtask1</v-list-item-title>
-          </v-list-item-content>
-        </template>
+      <v-list-item v-for="([title], i) in admins" :key="i" link>
+        <v-list-item-title v-text="title"></v-list-item-title>
+      </v-list-item>
+    </v-list-group>
+		<v-divider></v-divider>
+    <v-list-group no-action sub-group>
+      <template v-slot:activator>
+        <v-list-item-content>
+          <v-list-item-title>Subtask2</v-list-item-title>
+        </v-list-item-content>
+      </template>
 
-        <v-list-item v-for="([title], i) in admins" :key="i" link>
-          <v-list-item-title v-text="title"></v-list-item-title>
-
-          <!-- <v-list-item-icon>
-            <v-icon v-text="icon"></v-icon>
-          </v-list-item-icon> -->
-        </v-list-item>
-      </v-list-group>
-
-      <v-list-group no-action sub-group>
-        <template v-slot:activator>
-          <v-list-item-content>
-            <v-list-item-title>Subtask2</v-list-item-title>
-          </v-list-item-content>
-        </template>
-
-        <v-list-item v-for="subtask in subsubtasks" :key="subtask.id" link>
-          <v-list-item-title v-text="subtask.title"></v-list-item-title>
-
-          <!-- <v-list-item-icon>
-            <v-icon v-text="icon"></v-icon>
-          </v-list-item-icon> -->
-        </v-list-item>
-      </v-list-group>
+      <v-list-item v-for="subtask in subsubtasks" :key="subtask.id" link>
+        <v-list-item-title v-text="subtask.title"></v-list-item-title>
+      </v-list-item>
     </v-list-group>
   </v-list>
 </template>
 <script>
 export default {
   data: () => ({
-		subsubtasks: [
-			{
-				title: "subSubTask1"
-			},
-			{
-				title: "subSubTask2"
-			},
-		]
-	}),
+    subsubtasks: [
+      {
+        title: "subSubTask1",
+      },
+      {
+        title: "subSubTask2",
+      },
+    ],
+  }),
 };
 </script>
